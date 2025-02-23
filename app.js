194b368eeea7ac8fe851ff99a210d4f01dff600b
fronan13 <govno228por@mail.ru>
2025-02-23 21:44:07 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Базовые арифметические опператоры//
+
+const width = 5;
+const height = 10;
+const space = width * height ;
+const volume = 2**3;
+console.log(volume);
+
+//Строки 
+const citi = 'Moscow';
+const street = 'Novoslobotskya';
+console.log( citi + ' ,' + street );
