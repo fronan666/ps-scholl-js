275640400498e8f5888467ede40ca8f32a071c17
fronan13 <govno228por@mail.ru>
2025-03-08 20:51:01 +0700

    цикл в цикле

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,10 +1,21 @@
 
-const arr = ['!', 'JS', 'Люблю', 'Я'];
-const result = [];
-for(let i = arr.length - 1 ; i>=0 ; i--) {
-    result.push(arr[i]);
+    // for (let i = 1 ; i < 5 ;i++) {
+    //     console.log(`Цикл со значение:  ${i}`);
+    //     for(let j =1 ; j < 5; j++){
+    //         console.log(`Цикл 2 со значение:  ${j}`)
+    //     }
+    // }
+
+const tasks = [[1, 'Задача 1'],[2, 'Задача 2']];
+
+for (let i = 0 ; i < tasks.length ;i++) {
+    for(let j =0 ; j < tasks[i].length; j++){
+        console.log(tasks[i][j]);
+    }
+    
 }
-console.log(result.join(' '))
+
+
 
 
 
