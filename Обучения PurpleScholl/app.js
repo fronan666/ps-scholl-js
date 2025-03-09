b2ac2b94e7ee11ab5a7098687ddd6b23755b3c69
fronan13 <govno228por@mail.ru>
2025-03-09 17:31:17 +0700

    цикл while

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,19 +1,36 @@
 
-    // for (let i = 1 ; i < 5 ;i++) {
-    //     console.log(`Цикл со значение:  ${i}`);
-    //     for(let j =1 ; j < 5; j++){
-    //         console.log(`Цикл 2 со значение:  ${j}`)
-    //     }
-    // }
 
-const tasks = [[1, 'Задача 1'],[2, 'Задача 2']];
+// let i = 1;
+// while (i < 5) {
+//     console.log(`Цикл со значение:  ${i}`);
+//     i++;
+// }
 
-for (let i = 0 ; i < tasks.length ;i++) {
-    for(let j =0 ; j < tasks[i].length; j++){
-        console.log(tasks[i][j]);
+const arr = [1 ,4, 7, 8];
+
+for (let i = 0 ; i < arr.length ;i++) {
+    if(arr[i]>5) {
+        break;
     }
-    
+    console.log(arr[i]);
 }
+
+let i = 0
+while (arr[i] <= 5 && i < arr.length) {
+    console.log(arr[i]);
+    i++;
+}
+let j = 0;
+
+do  {
+    console.log(j);
+    j++;
+} while(j < 0) {
+
+}
+
+
+
 
 
 
