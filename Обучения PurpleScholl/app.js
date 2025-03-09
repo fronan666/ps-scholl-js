84509b601e05c114cfe52d2c8c4aaf209354efc2
fronan13 <govno228por@mail.ru>
2025-03-09 18:28:45 +0700

    for of for in

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,32 +1,18 @@
 
 
-// let i = 1;
-// while (i < 5) {
-//     console.log(`Цикл со значение:  ${i}`);
-//     i++;
-// }
-
 const arr = [1 ,4, 7, 8];
 
 for (let i = 0 ; i < arr.length ;i++) {
-    if(arr[i]>5) {
-        break;
-    }
     console.log(arr[i]);
 }
 
-let i = 0
-while (arr[i] <= 5 && i < arr.length) {
-    console.log(arr[i]);
-    i++;
+for(let elemetn of arr ) {
+    console.log(elemetn);
 }
-let j = 0;
 
-do  {
-    console.log(j);
-    j++;
-} while(j < 0) {
 
+for(let index in arr) {
+    console.log(arr[index]);
 }
 
 
