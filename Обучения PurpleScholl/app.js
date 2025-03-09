233b61037d2fb8bbccf68395b91420fc8ed73ed5
fronan13 <govno228por@mail.ru>
2025-03-09 21:23:31 +0700

    упражнения 2 циклы

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,22 +1,55 @@
 
 
-const arr = [1 ,4, 7, 8];
+const operations = [1000 ,-700, 300, -500, 10000];
+const startBalance = 100;
+function getBalance(arrayOfOperations, initialBalance) {
+    let balance = initialBalance;
+    for(const element of arrayOfOperations ) {
+    balance += element ;
+    }
 
-for (let i = 0 ; i < arr.length ;i++) {
-    console.log(arr[i]);
+    return balance ;
 }
+console.log(getBalance(operations , startBalance ));
 
-for(let elemetn of arr ) {
-    console.log(elemetn);
+function checkOperations (arrayOfOperations, initialBalance) {
+    let balance = initialBalance;
+    let isOk = true ;
+    for(const element of arrayOfOperations ) {
+        balance += element ;
+        if (balance < 0 ){
+            isOk = false ;
+            break;
+        }
+    }
+    return isOk ;
+
 }
+console.log(checkOperations(operations , startBalance ));
 
 
-for(let index in arr) {
-    console.log(arr[index]);
+function overageOperations(arrayOfOperations) {
+    let positiveCount = 0;
+    let positiveSum = 0;
+    let negativeCount = 0;
+    let negativeSum = 0;
+
+    for(const element of arrayOfOperations ) {
+        if( element > 0) {
+            positiveCount++ ;
+            positiveSum += element ;
+        }
+
+            if( element < 0){
+
+                negativeCount++ ;
+                negativeSum += element ;
+            }
+
+    }
+return [positiveSum / positiveCount ,negativeSum / negativeCount ];
 }
-
-
-
+console.log(overageOperations(operations));
 
 
 
