df148e54cf362e30cb1cfff7160fbb051112a659
fronan13 <govno228por@mail.ru>
2025-03-12 02:28:51 +0700

    возврат функции

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,30 +1,17 @@
-function add(a , b) {
-    return a+b ;
-}
-function subtract(a ,b) {
-    return a - b ;
-}
-
-
-function power(a ,b) {
-    return a**b
-}
-//функция высшего порядка
-
-function calculate( a ,b ,fn) {
-    console.log(fn.name);
-    const res = fn(a , b);
-    return res ;
+function power(pow) {
+    return function (num){
+        return num**pow;
+    }
 }
 
-let result = calculate(3 , 5 , subtract);
-console.log(result);
-result = calculate(3 , 5 , add);
-console.log(result);
-result = calculate(3 , 5 , power);
-console.log(result);
+const powerOfTwo = power(2); 
+console.log(powerOfTwo(5));
 
+const powerOfThree = power(3);
 
+console.log(powerOfThree(5));
+
+console.log(power(5)(4));
 
 
 
