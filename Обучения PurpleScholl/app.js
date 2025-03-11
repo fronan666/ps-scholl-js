2a87cc44233dcff5d639abc83ecd79b563da9c64
fronan13 <govno228por@mail.ru>
2025-03-12 02:20:22 +0700

    callback

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,29 +1,29 @@
-for (let i = 1; i < 3; i++) {
-    for (let j = 1; j < 3; j++) {
-        console.log(i);
-    }
+function add(a , b) {
+    return a+b ;
 }
-//Для уточнения, можешь привести простой пример вложенных циклов, 
-//например, используя массив чисел, чтобы, скажем, найти все
-// пары чисел, которые в сумме дают определённое значение?
-
-function findPairs(numbers, targetSum) {
-    const pairs = [];
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) { // начинаем с i + 1, чтобы не повторять пары
-            if (numbers[i] + numbers[j] === targetSum) {
-                pairs.push([numbers[i], numbers[j]]);
-            }
-        }
-    }
-    return pairs;
+function subtract(a ,b) {
+    return a - b ;
 }
 
-// Пример использования
-const numbers = [1, 2, 3, 4, 5];
-const targetSum = 5;
-const result = findPairs(numbers, targetSum);
-console.log(result); // Вывод: [[1, 4], [2, 3]]
+
+function power(a ,b) {
+    return a**b
+}
+//функция высшего порядка
+
+function calculate( a ,b ,fn) {
+    console.log(fn.name);
+    const res = fn(a , b);
+    return res ;
+}
+
+let result = calculate(3 , 5 , subtract);
+console.log(result);
+result = calculate(3 , 5 , add);
+console.log(result);
+result = calculate(3 , 5 , power);
+console.log(result);
+
 
 
 
