851a7b209e6479c755df567556b284b18109ba39
fronan13 <govno228por@mail.ru>
2025-03-16 00:19:59 +0700

    reduce

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,7 +1,28 @@
-const price = [[100, 200], [120, 100], [200, 350]];
+const operations = [100 , -20 , 7 , -30 , 50]; 
 
-const result  = price
-.map(product => product[1] - product[0])
-.filter(price => price > 0);
-console.log(result);
+let balance = 0;
 
+for(let operation of operations) {
+    balance += operation;
+
+}
+
+
+
+const finalBalance = operations.reduce((acc , operation, i) => {
+    console.log(`Итерация: ${i}, acc: ${acc}, operation: ${operation}`)
+    return acc += operation
+}, 0);
+console.log(finalBalance);
+
+//0 - acc = 0, value = 100
+//1 -acc = 100 , value = -20 и т.д
+const minElement = operations.reduce((acc , operation, i) => {
+ if(operation > acc) {
+    return acc ;
+ }else {
+    return operation;
+ }
+
+}, 0);
+console.log(minElement);
