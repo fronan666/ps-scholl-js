61bcfc32e7ccf4ba062e9109e8586f8066761072
fronan13 <govno228por@mail.ru>
2025-03-16 02:33:12 +0700

    дз нахожднеия some с помощью find

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,18 +1,11 @@
-const arr = [2, 4, 4, 10 ];
+const arr = [2, 4, 4, 10, 20];
+function some(array, element) {
+    const res = array.find(el => el===element)
+    return res == undefined? false : true ;
+}
+console.log(some(arr , 2));
+
+const result = arr.some(el => el === 2);
+console.log(result);
 
 
-
-const avg = arr.reduce((acc , el, i) => {
-    if(i != arr.length -1){
-        return acc + el;
-    }else {
-        return (acc + el)/ arr.length
-    }
-}, 0);
-
-console.log(avg);
-
-
-
-//0 - acc = 0, value = 100
-//1 -acc = 100 , value = -20 и т.д
