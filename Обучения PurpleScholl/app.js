b79ca6eb5dfa5b6fb6253a91d51e2b8b3820eff0
fronan13 <govno228por@mail.ru>
2025-03-21 21:17:51 +0700

    упр строки

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,12 +1,9 @@
-const userName = 'Васян Пупкина';
-console.log(userName)
-console.log(userName[0]);
-console.log(userName[0] + userName[1]);
-console.log(userName.charAt(2));
-console.log(userName.length);
-console.log(userName.indexOf('н'));
-console.log(userName.lastIndexOf('н'));
-console.log(userName.includes('н'))
-console.log(userName.slice(6));
-console.log(userName.slice(6 , 10));
+const fullUserName = 'Вася aka terminator Пупкин';
+
+const userName =  fullUserName.slice(0 , fullUserName.indexOf(' '));
+console.log(userName);
+
+const userSurname = fullUserName.slice(fullUserName.lastIndexOf ('П'), fullUserName.length );
+console.log(userSurname);
+
 
