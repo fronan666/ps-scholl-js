9b6c20d3f8b376faa407776172f88e4606b0add9
fronan13 <govno228por@mail.ru>
2025-04-08 14:03:37 +0700

    упр склад

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,20 +1,52 @@
-const cities = {
-  msk: {
-    temp: {
-      celsia: 26,
-    },
+const warehouse = {
+  goods: [],
+  findGoodById: function (id) {
+    return this.goods.find(g => g.id == id);
   },
-  spb: {
 
-  }
+  addGood: function (good) {
+    const exsistedGood = this.findGoodById(good.id)
+    if(exsistedGood) {
+      console.log('Этот товар уже есть на складе');
+      return;
+    }
+    this.goods.push(good)
+  },
+  getWeightKg: function () {
+    return this.goods.reduce((acc , el) =>{
+      acc += el.weight?.kg? el.weight.kg : 0 ;
+    }, 0 )
+  },
 };
 
-const city = 'spb'
-if(cities[city] && cities[city].temp) {
-  console.log(cities[city].temp.celsia);
+/* Товары */
+const car = {
+  id: 1,
+  weight: {
+  kg: 1000
+  },
+  brand: 'Ford'
 }
 
-console.log(cities[city]?.temp?.celsia);
+const chair = {
+  id: 2,
+  weight: {
+  kg: 2
+  }
+}
+
+const paper = {
+  id: 3,
+  color: 'red'
+}
+
+
+warehouse.addGood(car)
+warehouse.addGood(car)
+warehouse.addGood(chair)
+warehouse.addGood(paper)
+console.log(warehouse.goods)
+
 
 
 
