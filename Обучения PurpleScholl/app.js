6c154699f6e6d62ec12d9e6b5eee2ab3366471a7
fronan13 <govno228por@mail.ru>
2025-04-15 04:31:26 +0700

    Enhanced Object Literals для объектов

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,33 +1,18 @@
 'use strict';
+const b = 1;
 
-const company = {
-  name: 'ООО Агро',
-  employees : [
-    {
-      name: 'Света',
-      getName : function() {
-        return this.name;
-    }
-  }
-  ],
-  ceo: {
-    name : 'Вася',
-    getName : function() {
-      return this.name;
+  const a = {
+    b,
+    getB: function () {
+      return this.b;
     },
-  },
-
-  getName : function() {
-    return this.name;
-  },
-
+    getbAlt() {
+      
+    }
 };
 
-
-console.log(company.getName());
-console.log(company.ceo.getName());
-console.log(company.employees.map(employee => employee.getName()))
-
+console.log(a);
+console.log(a.getB())
 
 
 
