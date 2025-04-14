e6b13d739f635fa4b507e0f9ec75cd3993e4b4a8
fronan13 <govno228por@mail.ru>
2025-04-15 05:28:23 +0700

    bind

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -2,37 +2,33 @@
 const audi = {
   make: 'Audi',
   model: 'A5',
-  year: 2024,
-  damages: [],
-  addDamage(part, rate ) {
-    console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено - повреждение : ${part} со степенью ${rate}`);
+  damages: []
+};
+
+const carManipulation = {
+  addDamage(part, rate) {
     this.damages.push({
-      part,
-      rate
+        part,
+        rate
     })
+    console.log(`Добавили повреждения на ${this.make} ${this.model}`);
   }
 };
 
 
-// audi.addDamage('капот', 1);
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('Крыло', 3) ;
+console.log(audi);
 
-const bmw = {
-  make: 'BMW',
-  model: '320i',
-  year: 2022,
-  damages: [],
-};
-bmw.addDamage = audi.addDamage;
-// bmw.addDamage('Бампер', 2);
-
-const addDamageFunction = audi.addDamage;
-// addDamageFunction('Бампер', 2); не работает
-addDamageFunction.call(bmw , 'Бампер', 2 );
-addDamageFunction.call(audi , 'капот', 1);
+const addDamageAudiRoof = carManipulation.addDamage.bind(audi , 'Крыша');
+addDamageAudiRoof(5);
+console.log(audi);
 
 
-addDamageFunction.apply(bmw, ['капот', 3]);
-addDamageFunction.apply(audi, ['бампер', 2]);
+
+
+
+
 
 
 
