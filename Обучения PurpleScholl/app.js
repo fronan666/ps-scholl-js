0eef22428d350713de99f713717f94f016bb03a6
fronan13 <govno228por@mail.ru>
2025-04-14 17:02:56 +0700

    контекст в методах

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,20 +1,38 @@
-addUser();
+'use strict';
 
-console.log(b);
-const a = 3 ;
-var b = 2 ;
-console.log(b);
-function addUser() {
+const user = {
+  firstName: 'Слава',
+  lastName: 'Воронцов',
+  age: 20 ,
+  getUserInfo:function() {
+    console.log(this);
+    console.log(`${this.firstName}  ${this.lastName}`);
+    
+    const canDrink = () => {
+      if(this.age >= 18) {
+        console.log('Может уже пить!');
+      } else {
+        console.log('Не может пить');
+      }
+    }
+      canDrink();
 
-  // console.log('user added');
-}
+  },
 
-const arr1 = () => {
-  console.log('arr1');
-}
-arr1();
+  getUserInfoArrow:() => {
+    console.log(this);
+    console.log(`${this.firstName}  ${this.lastName}`);
+  },
+};
 
-addUser();
+user.getUserInfo();
+user.getUserInfoArrow();
+
+
+
+
+
+
 
 
 
