29acd5887c5443e652cf41a665221f9ef3192c0a
fronan13 <govno228por@mail.ru>
2025-04-15 05:10:18 +0700

    apply , call

--- Обучения PurpleScholl/app.js
+++ Обучения PurpleScholl/app.js
@@ -1,19 +1,38 @@
 'use strict';
-const b = 1;
-
-  const a = {
-    b,
-    getB: function () {
-      return this.b;
-    },
-    getbAlt() {
-      
-    }
+const audi = {
+  make: 'Audi',
+  model: 'A5',
+  year: 2024,
+  damages: [],
+  addDamage(part, rate ) {
+    console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено - повреждение : ${part} со степенью ${rate}`);
+    this.damages.push({
+      part,
+      rate
+    })
+  }
 };
 
-console.log(a);
-console.log(a.getB())
 
+// audi.addDamage('капот', 1);
+
+const bmw = {
+  make: 'BMW',
+  model: '320i',
+  year: 2022,
+  damages: [],
+};
+bmw.addDamage = audi.addDamage;
+// bmw.addDamage('Бампер', 2);
+
+const addDamageFunction = audi.addDamage;
+// addDamageFunction('Бампер', 2); не работает
+addDamageFunction.call(bmw , 'Бампер', 2 );
+addDamageFunction.call(audi , 'капот', 1);
+
+
+addDamageFunction.apply(bmw, ['капот', 3]);
+addDamageFunction.apply(audi, ['бампер', 2]);
 
 
 
